41286e83d6f1e7da1cd25a8fe34a3d2103226db3
Robi <robi.maliqi@hotmail.co.uk>
2022-07-18 21:45:40 +0100

    added a 3 validation checks, one to make sure all fields are filled in, one to make sure passwords mathc and one for password length, also one to make sure that password if more than 0 length

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,26 @@ router.post('/register', (req, res) =>{
   const { name, email, password, password2 } = req.body;
   let errors = [];
 
+  //Check required fields
+  if(!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please fill in all field'});
+  }
 
+  //Check passwords match
+  if(password !== password2) {
+    errors.push({ msg: 'Passwords do not match'});
+  }
+
+  //Check pass length
+  if(password.length < 6) {
+    errors.push({ msg: 'Password should be at least 6 characters'});
+  }
+
+  if(errors.length > 0) {
+
+  } else {
+    res.send('pass');
+  }
 
 });
 
