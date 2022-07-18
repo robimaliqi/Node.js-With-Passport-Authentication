7b5824b00da4f1d1cd5804ead7d55d48b78787f8
Robi <robi.maliqi@hotmail.co.uk>
2022-07-18 21:39:57 +0100

    set up Bodyparser in app.js and register handle in users.js

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,13 @@ router.get('/login', (req, res) => res.render('login'));
 //Register Page
 router.get('/register', (req, res) => res.render('register'));
 
+// Register Handle
+router.post('/register', (req, res) =>{
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+
+
+});
+
 module.exports = router;
